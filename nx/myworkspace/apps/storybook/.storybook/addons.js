9c3c0eb6c2bf4d514a29bf3349ee7900e39e5fcc
vorant <twojuly89@gmail.com>
2019-04-22 09:52:08 +0800

    add llinks

--- nx/myworkspace/apps/storybook/.storybook/addons.js
+++ nx/myworkspace/apps/storybook/.storybook/addons.js
@@ -4,4 +4,5 @@ import '@storybook/addon-a11y/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-notes/register';
 import '@storybook/addon-viewport/register';
+import '@storybook/addon-links/register';
 
--- nx/myworkspace/apps/storybook/src/stories/index.stories.ts
+++ nx/myworkspace/apps/storybook/src/stories/index.stories.ts
@@ -1,16 +1,24 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
 import { Button } from '@storybook/angular/demo';
+import { linkTo } from '@storybook/addon-links';
 
 storiesOf('My Button', module)
-    .add('with text', () => ({
+    .add('First button', () => ({
         component: Button,
         props: {
-            text: 'Hello Button'
+            text: 'My Button',
         }
     }))
     .add('with emoji', () => ({
         component: Button,
         props: {
             text: '😀 😎 👍 💯'
+        }
+    }))
+    .add('Go to My Button', () => ({
+        template:
+            '<button (click)="linkTo()">Link</button>',
+        props: {
+            linkTo: linkTo('MyButtonComponent', 'My button Angular component')
         }
     }));
